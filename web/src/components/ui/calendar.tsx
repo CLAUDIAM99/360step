b1db38b8bd63ca924111a7d13dffe77feb1d312b
CLAUDIAM99 <claudia.manicone99@gmail.com>
2026-04-11 17:58:56 +0200

    Update calendar.tsx

--- web/src/components/ui/calendar.tsx
+++ web/src/components/ui/calendar.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import * as React from "react";
-import { ChevronLeft, ChevronRight } from "lucide-react";
+import {
+  ChevronDown,
+  ChevronLeft,
+  ChevronRight,
+  ChevronUp,
+} from "lucide-react";
 import { DayPicker } from "react-day-picker";
 import { cn } from "@/lib/utils";
 
@@ -12,8 +17,21 @@ function Calendar({ className, ...props }: CalendarProps) {
     <DayPicker
       className={cn("p-3", className)}
       components={{
-        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
-        IconRight: () => <ChevronRight className="h-4 w-4" />,
+        Chevron: ({ orientation, className: iconClass, size }) => {
+          const c = cn("h-4 w-4", iconClass);
+          switch (orientation) {
+            case "left":
+              return <ChevronLeft className={c} size={size} />;
+            case "right":
+              return <ChevronRight className={c} size={size} />;
+            case "up":
+              return <ChevronUp className={c} size={size} />;
+            case "down":
+              return <ChevronDown className={c} size={size} />;
+            default:
+              return <ChevronLeft className={c} size={size} />;
+          }
+        },
       }}
       {...props}
     />
